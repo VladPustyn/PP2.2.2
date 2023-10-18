16e43c99272975177567b3574e35e52ecaed76d2
Vladis <pustynnikov966@mail.ru>
2023-10-18 12:47:41 +0300

    second

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,14 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.config.service.CarService;
-import web.config.service.CarServiceImp;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,8 +1,7 @@
-package web.config.service;
+package web.service;
 
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface CarService {
